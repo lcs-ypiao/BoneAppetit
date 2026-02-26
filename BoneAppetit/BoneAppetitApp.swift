3be13545d60b61cabff88558f907a5320863c98e
Yolanda Piao <ypiao@lcs.on.ca>
2026-02-26 08:59:59 -0500

    Initial Commit

---
+++ BoneAppetit/BoneAppetitApp.swift
@@ -0,0 +1,17 @@
+//
+//  BoneAppetitApp.swift
+//  BoneAppetit
+//
+//  Created by Yolanda Piao on 2/26/26.
+//
+
+import SwiftUI
+
+@main
+struct BoneAppetitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
